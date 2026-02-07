1b7b9974b34a3903f7ab9992f88c1f5937114be8
DAYYAN2008 <dayyanyasir@gmail.com>
2026-02-07 09:11:24 +0500

    Update relay to Replit dev url

--- packages/client/src/lib/gun.ts
+++ packages/client/src/lib/gun.ts
@@ -4,6 +4,7 @@ import Gun from 'gun';
 // These are free, community-run servers that just bounce signals.
 const gun = Gun({
   peers: [
+   'https://3f1fb3bf-99e3-415b-83ae-8821402ae4dd-00-cau89310wj6w.sisko.replit.dev/gun',
     'https://gun-manhattan.herokuapp.com/gun', // The main community relay
     'https://relay.1234.as/gun',              // Backup relay
     'https://gun-us.herokuapp.com/gun'        // Backup relay
